6807ef0432eb024f8e9a5554017563ad2953aa85
Abhishek37k <iabhishek6kumar@gmail.com>
2025-07-06 19:26:49 +0530

    Re-added 1first_project_calender_and_title_change as regular folder

---
+++ 1first_project_calender_and_title_change/src/components/Expenses.jsx
@@ -0,0 +1,21 @@
+// Write your code here
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          price={expense.price}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
